198549c03a19d247b5eefe37842b60fcd44461b0
floxydio <diooktar@gmail.com>
2023-01-20 22:36:10 +0700

    add comment

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,22 +1,25 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../database/orm"
-import { UserModel } from "../models/users.model"
+import { UserModel } from "../models/users.model" // const userModel = require("../models/users.model") -> JS
+import bcrypt from "bcrypt" // -> const bcrypt = require("bcrypt") -> JS
+
 
 export async function SignIn(req: Request, res: Response) {
-   // const userRepository = AppDataSource.getRepository(UserModel)
+   const userRepository = AppDataSource.getRepository(UserModel) // -> Wajib Dipanggil untuk Repository
 
+   const resultData = await userRepository.find({
+      where: {
+         username: req.body.username
+      }
+   }) // User Login ngetik -> Username & Password
+   let resultBcrypt = bcrypt.compareSync(req.body.password, resultData[0].password!); // Untuk cek dari req.body.password dengan tabel database password
 
-   // await userRepository.find({
-   //    where: {
-   //       name: req.body.name
-   //    }
-   // }).catch((error) => {
-   //    return res.status(400).send(error)
-   // }).then((data) => {
-   //    return res.send(data)
-   // })
-
-
+   // Jika ResultBcrypt == true -> Benar / Sesuai, false -> Salah
+   if (resultBcrypt) {
+      return res.status(200).send("Succesfully Login")
+   } else {
+      return res.status(400).send("Username or Password Incorrect")
+   }
 }
 
 export async function SignUp(req: Request, res: Response) {
--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -20,6 +20,3 @@ export class UserModel {
 
 }
 
-// Test
-
-// Test Nambahin
