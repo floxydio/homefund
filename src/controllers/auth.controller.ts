45e79ddcaed2959939ef4a4f012183143c58b159
floxydio <diooktar@gmail.com>
2023-01-20 22:15:51 +0700

    add migration test

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,13 +1,36 @@
 import { Request, Response } from "express";
+import { AppDataSource } from "../database/orm"
+import { UserModel } from "../models/users.model"
 
-export function SignIn(req: Request, res: Response) {
+export async function SignIn(req: Request, res: Response) {
+   // const userRepository = AppDataSource.getRepository(UserModel)
+
+
+   // await userRepository.find({
+   //    where: {
+   //       name: req.body.name
+   //    }
+   // }).catch((error) => {
+   //    return res.status(400).send(error)
+   // }).then((data) => {
+   //    return res.send(data)
+   // })
+
 
 }
 
-export function SignUp(req: Request, res: Response) { }
+export async function SignUp(req: Request, res: Response) {
+   // const userRepository = AppDataSource.getRepository(UserModel)
 
-export function Logout(req: Request, res: Response) { }
+   // await userRepository.create({
+   //    name: req.body.name,
+   //    password: req.body.password,
+   //    profile_image: req.body.profile_image
+   // })
+}
 
-export function EditProfile(req: Request, res: Response) { }
+export async function Logout(req: Request, res: Response) { }
 
-export function ProfileCheck(req: Request, res: Response) { }
+export async function EditProfile(req: Request, res: Response) { }
+
+export async function ProfileCheck(req: Request, res: Response) { }
