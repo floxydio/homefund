f688f6a759e5a7c1f7dbe2855e041e35892fa598
YudYudz <arthadiaz1@gmail.com>
2023-01-21 14:20:01 +0700

    SignIn, SignUp, Jsonwebtoken

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,63 @@
+import bcrypt, { hash } from 'bcrypt';
+import { UserModel } from './../models/users.model';
+import { AppDataSource } from './../../kontrakdana-be/src/database/orm';
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken"
 
-export function SignIn(req: Request, res: Response) {
+export async function SignIn(req: Request, res: Response) { 
+  const userRepository = AppDataSource.getRepository(UserModel)
 
+  const resultData = await userRepository.find({
+    where: {
+      username: req.body.username
+    }
+  });
+  let resultBcrypt = bcrypt.compareSync(req.body.password, resultData[0].password!);
+
+  if (resultBcrypt) {
+    let token = jwt.sign(
+      {
+        data: {
+          id: resultData[0].id,
+          username: resultData[0].username,
+        },
+      },
+      "secret", { expiresIn: '1h' }
+    );
+    return res.status(200).send({
+      data: {
+        id: resultData[0].id,
+        username: resultData[0].username,
+        name: resultData[0].name,
+      },
+      token: token,
+    });
+  } else {
+    res.status(400).send({
+      message: "Username or Password Invalid",
+    });
+  }
 }
 
-export function SignUp(req: Request, res: Response) { }
+export async function SignUp(req: Request, res: Response) { 
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
+  const userRepository = AppDataSource.getRepository(UserModel)
+  const resultData = userRepository.create({
+    name: req.body.name,
+    username: req.body.username,
+    password: hash,
+  });
+
+res.status(201).send({
+  status: 201,
+  data: resultData,
+  message: "Succesfully Create Account",
+});
+  
+}
 
 export function Logout(req: Request, res: Response) { }
 
---
+++ src/middleware/auth.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export default function authenticatetoken(req:Request, res:Response, next: NextFunction) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    return res.status(401).json({
+      message: 'Token Not Found',
+    });
+  }
+  jwt.verify(token, 'secret', (err, result) => {
+    if (err) {
+      return res.status(401).json({
+        message: 'User Unauthorized',
+      });
+    }
+  });
+
+  next();
+}
+
