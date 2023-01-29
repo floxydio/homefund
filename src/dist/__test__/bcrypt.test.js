9c950d3aabef4dcde46460c7c5a2371d9d1273fc
floxydio <diooktar@gmail.com>
2023-01-30 00:52:59 +0700

    update

--- src/dist/__test__/bcrypt.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const bcrpyt = require("bcrypt");
-describe("Test Bcrypt", () => {
-    it("Should Be True", () => {
-        let data = "abc";
-        let result = bcrpyt.compareSync(data, "");
-        // expect("abc", "");
-    });
-});
